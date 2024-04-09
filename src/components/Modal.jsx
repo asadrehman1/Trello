280c81abe5de2edd798529d6c7d55cdccd38007e
Your Name <asadrehman1>
2024-04-09 12:36:25 -0400

    fixed the Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -20,7 +20,7 @@ const Modal = ({ onClose, children }) => {
   return (
     <div
       ref={modalRef}
-      className="h-[100vh] p-5 shadow-md w-[800px] absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] rounded-lg bg-zinc-900 z-10000"
+      className="min-h-[50vh] p-5 shadow-md w-[50] absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] rounded-lg bg-zinc-900 z-10000"
     >
       {children}
     </div>
